671c50fb1f6619650201cf7f041061e21cf5d62a
TahiryAllaoui <tallaoui31@gmail.com>
2023-08-05 19:31:25 +0300

    Responsive Navbar done

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <div className='app'>
+      <div id='back'></div>
       <Navbar />
       <Section />
       <Footer />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,32 @@ import logo from '../assets/images/logo.svg'
 import '../style/Navbar.scss'
 
 const Navbar = () => {
+    const handleOpen = () => {
+        let nav = document.querySelector('.navbar-item') as HTMLElement;
+        nav.style.left = "42%";
+        let back = document.querySelector('#back') as HTMLElement;
+        back.style.display = "block";
+    }
+    const handleClose = () => {
+        let nav = document.querySelector('.navbar-item') as HTMLElement;
+        nav.style.left = "calc(100% + 1rem)";
+        let back = document.querySelector('#back') as HTMLElement;
+        back.style.display = "none";
+    }
     return <nav className='navbar-main'>
-        <img src={logo} alt="" />
+        <div className='header-nav'>
+            <img src={logo} alt="" />
+            <img src={menuOpen} alt="" className='menu-open' onClick={handleOpen} />
+        </div>
         <div className="navbar-item">
-            <a href="#">Home</a>
-            <a href="#">New</a>
-            <a href="#">Popular</a>
-            <a href="#">Trending</a>
-            <a href="#">Categories</a>
+            <img src={menuCLose} alt="" className='menu-close' onClick={handleClose} />
+            <div className="inner-navbar">
+                <a href="#">Home</a>
+                <a href="#">New</a>
+                <a href="#">Popular</a>
+                <a href="#">Trending</a>
+                <a href="#">Categories</a>
+            </div>
         </div>
     </nav>
 };
